2e677aa52dfaadbf20a677d717ccad52d8bfaa09
Gow1621 <119664326+Gow1621@users.noreply.github.com>
2022-12-05 17:52:39 +0530

    Update conn.php

--- conn.php
+++ conn.php
@@ -1,3 +1,3 @@
 <?php 
-	$con = mysqli_connect('localhost','admin','admin123','studentdb');
+	$con = mysqli_connect('database-1.cjct0zv8xwmi.us-east-1.rds.amazonaws.com','admin','admin123','studentdb');
  ?>]
